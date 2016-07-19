663e2ca16d69fcd88127dc990421625eb7b67ff2
Enrique Carral Trevino <ecarralt@gmail.com>
2016-07-18 20:59:28 -0500

    changed photos to instance variable within create method

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -9,22 +9,22 @@ class PhotosController < ApplicationController
   end
 
   def create
-    photo = Photo.new
-    photo.title = params[:photo][:title]
-    photo.location = params[:photo][:location]
-    photo.vacation_id = params[:photo][:vacation_id]
-    photo.photofile = params[:photo][:photofile]
+    @photo = Photo.new
+    @photo.title = params[:photo][:title]
+    @photo.location = params[:photo][:location]
+    @photo.vacation_id = params[:photo][:vacation_id]
+    @photo.photofile = params[:photo][:photofile]
 
 
-    results = JSON.parse(Http.get("http://locationiq.org/v1/search.php?key=7b515e5d2f0389b8972f&format=json&q=#{CGI::escape(photo.location)}").body)
+    results = JSON.parse(Http.get("http://locationiq.org/v1/search.php?key=7b515e5d2f0389b8972f&format=json&q=#{CGI::escape(@photo.location)}").body)
 
     photolocation= results.first
-    photo.latitude = photolocation["lat"]
-    photo.longitude = photolocation["lon"]
+    @photo.latitude = photolocation["lat"]
+    @photo.longitude = photolocation["lon"]
     # photo.latitude = params[:photo][:latitude]
     # photo.longitude = params[:photo][:longitude]
 
-    if photo.save
+    if @photo.save
       redirect_to home_path
     else
       redirect_to new_photo_path, notice: "Error, please enter info in all the fields!"
